bcd478649dced21596d1cc82ac35f27816699405
Joyce Oh <joyce.oh@live.com>
2024-09-07 12:17:24 -0400

    member file working team file is having issues

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-
+    <section>talk about the application and how it works</section>
 </template>
 
 <style lang="css">
---
+++ src/components/Team/Team.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Member from "./Member.vue"
+</script>
+
+<template>
+  <section>
+    <Member></Member>
+    <Member></Member>
+    <Member></Member>
+    <Member></Member>
+    <Member></Member>
+  </section>
+</template>
+
+<style scoped>
+  section {
+    margin: 10em 1em;
+    border: .5em solid;
+    border-radius: 2em;
+    padding: 2em;
+
+    display: flex;
+    flex-wrap:wrap;
+    /* justify-content: center; */
+    justify-content: space-evenly;
+  }
+</style>
