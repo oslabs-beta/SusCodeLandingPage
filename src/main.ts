a4d2d15ca8e146fdc8ee64b694807765d4609717
Seth Hollingsworth <>
2024-09-11 13:15:23 -0700

    changes for build

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+// @ts-ignore
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
 import './style.css'
+// @ts-ignore
 import BootstrapVue3 from 'bootstrap-vue-3';
 
 createApp(App).use(BootstrapVue3).mount('#app')
